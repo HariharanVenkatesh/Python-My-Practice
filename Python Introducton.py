a74b52d3bfdb5ea2e3352bf636a1dc1405b282b1
HariharanVenkatesh <harihaniscristo23@gmail.com>
2024-08-02 17:05:23 +0530

    intro

---
+++ Python Introducton.py
@@ -0,0 +1,27 @@
+"""
+This is a
+example of
+multiple lines
+of comment
+adding new lines
+"""
+#-Used for Single line Comment
+
+
+#Variables & Data Types
+a = -1346789887765               #int
+b = 282364.87343                 #Float
+c = 'Hariharan @ 123 #!'         #String
+d = False                        #Boolean
+
+#print () Python Standard Function
+print (a)
+print (b)
+print (c)
+print (d)
+print (a,b,c,d)
+
+#type ()- python standard function
+
+a = 10
+print (a, type(a))
