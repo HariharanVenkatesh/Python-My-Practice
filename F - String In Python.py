61ddcd89aa6796e72c618e6a4cdce79ae1951081
HariharanVenkatesh <harihaniscristo23@gmail.com>
2024-08-02 18:06:32 +0530

    f - string

---
+++ F - String In Python.py
@@ -0,0 +1,15 @@
+#f - Strings
+#without using f-String
+
+name= 'Hariharan.V' 
+age= 22
+height= 1.6
+print("My name is ",name, "I am ", age, "years old", "My height is ", height, "meters")
+
+#After Using f-String
+name= 'Hariharan.V' 
+age= 22
+height= 1.6
+print(f"My name is {name} .I am  {age} years old. My height is  {height} meters")
+print(f"Hariharan's father is {age*2} years old.")
+
