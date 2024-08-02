e26e5119fd28b35b692e355aed6c21a45919139f
HariharanVenkatesh <harihaniscristo23@gmail.com>
2024-08-02 17:23:30 +0530

    If&else

---
+++ if&else Conditional.py
@@ -0,0 +1,89 @@
+# If else - descision statement
+# Syntax
+
+# if <condition T/F>:                #(compulsary)
+# elif <condition T/F:               #(optional) (as many as you want)
+# else:                              #(optional)
+
+a = 5
+if a>5:
+    print(a,"is bigger than 5")
+    print("code inside if")
+    print("my demo code")
+elif a == 5:
+    print(a,"is equal to 5")
+else:
+    print(a,"is smaller than 5")
+    print("Hello World")
+print("code")
+
+
+#if&if Else Conditional With Example Program
+
+height = int(input("Enter the Height in feet: "))
+
+if height < 5:
+    print("Buy a Token")
+    print("Now You Can Board the Metro")
+    print("You Don't have any tokens")
+else :
+    print("You Already have a Token")
+print("Where is your token")    
+
+
+height = int(input("Enter the Height in feet: "))
+
+if height > 5:
+    print("Buy a Token")
+    print("Now You Can Board the Metro")
+    print("You Don't have any tokens")
+else :
+    print("You Already have a Token")
+print("Where is your token")    
+
+
+height = int(input("Enter the Height in feet: "))
+
+if height <= 5:
+    print("Buy a Token")
+    print("Now You Can Board the Metro")
+    print("You Don't have any tokens")
+else :
+    print("You Already have a Token")
+print("Where is your token")    
+
+
+height = int(input("Enter the Height in feet: "))
+
+if height >= 5:
+    print("Buy a Token")
+    print("Now You Can Board the Metro")
+    print("You Don't have any tokens")
+else :
+    print("You Already have a Token")
+print("Where is your token")    
+
+
+height = int(input("Enter the Height in feet: "))
+
+if height == 5:
+    print("Buy a Token")
+    print("Now You Can Board the Metro")
+    print("You Don't have any tokens")
+else :
+    print("You Already have a Token")
+print("Where is your token")    
+
+
+height = int(input("Enter the Height in feet: "))
+
+if height != 5:
+    print("Buy a Token")
+    print("Now You Can Board the Metro")
+    print("You Don't have any tokens")
+else :
+    print("You Already have a Token")
+print("Where is your token")    
+
+height = int(input("Enter the Height in feet: "))
+
