249eb14498b9d0dc964aee9b9d6f547a1fdd4a19
HariharanVenkatesh <harihaniscristo23@gmail.com>
2024-08-02 17:49:37 +0530

    Func with Args

---
+++ Functions With Argument In Python.py
@@ -0,0 +1,23 @@
+#Functions in Python(Define Function,Call function)
+
+#(2 types of functions: build-in,user-defined) excersices done in reeborg's world
+
+#User-Defined Function
+def greet():
+    print("Hey")
+    print("Hariharan")
+greet()
+
+
+#Function with arguements in python
+
+def some(name):
+    print(f"Hi {name}")
+    print("How are you ?")
+some("Hariharan") 
+
+
+def add(a,b):
+    c = a + b
+    print(f"sum is {c}")
+add(10,10)
