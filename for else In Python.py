6e1699d6ac8a89e91dde4bbeff900603361f2d84
HariharanVenkatesh <harihaniscristo23@gmail.com>
2024-08-02 17:35:07 +0530

    for else

---
+++ for else In Python.py
@@ -0,0 +1,15 @@
+#For else in Python
+tuple = (23,8,11,7,18,22)
+for i in tuple:
+    #print(i)
+    if i % 6 == 0:
+        print(i)
+        break
+    #else:
+     #   print("There is no number by 6 in this sequence !!")
+   # if i == 7:
+    #    break
+#else:
+    print("There is no number by 6 in this sequence !!")
+#    print("Loop is successfully completed and we are in else block now !!!")
+#print("Out of for/else")
